5b8801c2bb2d4d15164ad8ce93eef63cf4d9d69a
ryokosuge <ryo.kosuge@gmail.com>
2024-08-29 15:55:47 +0900

    chore: Update AWS instance tags and add RDS module for database setup

--- main.tf
+++ main.tf
@@ -13,15 +13,6 @@ provider "aws" {
   region = "us-west-2"
 }
 
-module "rds-ryokosuge" {
-  source  = "app.terraform.io/MIXI/rds-ryokosuge/aws"
-  version = "1.0.0"
-  # insert required variables here
-  db_name     = "Handson_ryokosuge"
-  db_password = "test1234"
-  db_username = "root"
-}
-
 resource "aws_instance" "app_server" {
   ami           = "ami-05134c8ef96964280"
   instance_type = "t2.micro"
