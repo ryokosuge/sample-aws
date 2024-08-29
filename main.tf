95dd11f3108560ba0c733ece210ee0dcacaa87eb
ryokosuge <ryo.kosuge@gmail.com>
2024-08-29 15:37:02 +0900

    chore: update AWS instance tags

--- main.tf
+++ main.tf
@@ -18,7 +18,9 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Handson_ryokosuge"
-    TED  = "20240829_Handson"
+    Name        = "Handson_ryokosuge"
+    TED         = "20240829_Handson"
+    Project     = "Handson"
+    Environment = "test"
   }
 }
